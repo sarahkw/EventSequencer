101d75bfb2b596216fcdf47d194ace83720def11
Sarah Wong <netcatkate@gmail.com>
2018-06-28 21:02:31 +0000

    FramesAndSeconds: Frames, Seconds, Minutes, Hours

--- eventsequencerlib/framesandseconds.cpp
+++ eventsequencerlib/framesandseconds.cpp
@@ -7,7 +7,36 @@ FramesAndSeconds::FramesAndSeconds(QObject *parent) : QObject(parent)
 
 QString FramesAndSeconds::framesToSeconds(int fps, int frames)
 {
-    int seconds = frames / fps;
-    int secondsLeftOver = frames % fps;
-    return QString("%1+%2").arg(seconds).arg(secondsLeftOver);
+    int f = frames;
+
+    const int hours = f / (fps * 60 * 60);
+    f %= (fps * 60 * 60);
+
+    const int minutes = f / (fps * 60);
+    f %= (fps * 60);
+
+    const int seconds = f / fps;
+    f %= fps;
+
+    const int framesLeft = f;
+
+    const bool hasMinutes = minutes > 0;
+
+    QString s = QString("%1+%2")
+                    .arg(seconds, hasMinutes ? 2 : 0, 10, QLatin1Char('0'))
+                    .arg(framesLeft);
+
+    const bool hasHours = hours > 0;
+
+    if (hasMinutes) {
+        s = QString("%1:%2")
+                .arg(minutes, hasHours ? 2 : 0, 10, QLatin1Char('0'))
+                .arg(s);
+    }
+
+    if (hasHours) {
+        s = QString("%1:%2").arg(hours).arg(s);
+    }
+
+    return s;
 }
--- eventsequencertest/tst_framesandseconds.h
+++ eventsequencertest/tst_framesandseconds.h
@@ -5,11 +5,41 @@
 
 using namespace testing;
 
-TEST(FramesAndSeconds, BasicFormat)
+
+
+// https://stackoverflow.com/a/42597727
+QT_BEGIN_NAMESPACE
+inline void PrintTo(const QString &qString, ::std::ostream *os)
+{
+    *os << qUtf8Printable(qString);
+}
+QT_END_NAMESPACE
+
+
+
+TEST(FramesAndSeconds, ToSeconds_Seconds)
 {
     const int fps = 5;
     EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps, 0), "0+0");
     EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps, 1), "0+1");
     EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps, 5), "1+0");
     EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps, 6), "1+1");
+}
+
+TEST(FramesAndSeconds, ToSeconds_Minutes)
+{
+    const int fps = 5;
+    EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps, fps * 60 * 1), "1:00+0");
+    EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps, fps * 60 * 1 + fps * 5), "1:05+0");
+    EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps, fps * 60 * 1 + fps * 5 + 1), "1:05+1");
+}
+
+TEST(FramesAndSeconds, ToSeconds_Hours)
+{
+    const int fps = 5;
+    EXPECT_EQ(FramesAndSeconds::framesToSeconds(fps,
+                                                fps * 60 * 60 * 1 +
+                                                fps * 60 * 1 +
+                                                fps * 5 +
+                                                1), "1:01:05+1");
 }
