afda34b5a77508379a9e7b5b90fa7b8dc3f52c1c
Sarah Wong <netcatkate@gmail.com>
2018-07-04 07:29:23 +0000

    Register QML type for BadJs

--- EventSequencer/registerqmltypes.cpp
+++ EventSequencer/registerqmltypes.cpp
@@ -3,12 +3,14 @@
 #include "document.h"
 #include "strip.h"
 #include "framesandseconds.h"
+#include "stripext/badjs.h"
 
 #include <QQmlEngine>
 
 void RegisterQmlTypes::registerQmlTypes()
 {
     qmlRegisterType<Document>("eventsequencer", 1, 0, "Document");
+    qmlRegisterType<stripext::BadJs>("eventsequencer.stripext", 1, 0, "BadJs");
     qmlRegisterType<Strip>();
     qmlRegisterSingletonType<FramesAndSeconds>(
         "eventsequencer", 1, 0, "FramesAndSeconds",
