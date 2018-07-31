bfcf18e2680c6f3105f534517c85ed3686f85f88
Sarah Wong <netcatkate@gmail.com>
2018-07-31 23:14:37 +0000

    There's a PlayerControl now I guess. Untested.

--- eventsequencerlib/playercontrol.cpp
+++ eventsequencerlib/playercontrol.cpp
@@ -1,11 +1,104 @@
 #include "playercontrol.h"
 
+#include "audioformatholder.h"
+#include "sessionaudio.h"
+
+#include <QAudioOutput>
+
+#include <QDebug>
+#include <QBuffer>
+
+void PlayerControl::play()
+{
+    if (audioFormatHolder_ == nullptr || audioOutput_ == nullptr) {
+        qWarning() << "Not ready";
+        return;
+    }
+    if (audioOutput_->state() != QAudio::StoppedState) {
+        qWarning() << "Not stopped";
+    }
+
+    audioOutput_->start(new QBuffer(this));  // TEMPORARY!
+    updateAudioState();
+}
+
+void PlayerControl::stop()
+{
+    if (audioOutput_ == nullptr) {
+        return;
+    }
+
+    if (audioOutput_->state() != QAudio::StoppedState) {
+        audioOutput_->stop();
+    }
+}
+
 void PlayerControl::updateAudioObject()
 {
+    QStringList errors;
+    if (audioFormatHolder_ == nullptr) {
+        errors.push_back("Audio format missing");
+    }
+    if (sessionAudio_ == nullptr) {
+        errors.push_back("Session audio missing");
+    }
 
+    if (audioOutput_ != nullptr) {
+        stop();
+        delete audioOutput_;
+        audioOutput_ = nullptr;
+        updateAudioState();
+    }
+
+    if (errors.empty()) {
+        Q_ASSERT(audioOutput_ == nullptr);
+        QAudioFormat qaf = audioFormatHolder_->toQAudioFormat();
+        audioOutput_ = new QAudioOutput(sessionAudio_->selectedOutputDevice(), qaf);
+
+        QObject::connect(audioOutput_, &QAudioOutput::stateChanged,
+                         this, &PlayerControl::updateAudioState);
+
+        if (!sessionAudio_->selectedOutputDevice().isFormatSupported(qaf)) {
+            errors.push_back("WARN: Device does not support format");
+        }
+    }
+
+    audioOutputReadyStatus_ = errors.join(", ");
+    emit audioOutputReadyStatusChanged();
+}
+
+void PlayerControl::updateAudioState()
+{
+    if (audioOutput_ == nullptr) {
+        setAudioState(AudioControl::AudioState::Unset);
+        setError("");
+        return;
+    }
+
+    setAudioState(audioOutput_->state());
+    setError(audioOutput_->error());
 }
 
 PlayerControl::PlayerControl(QObject* parent) : AudioControl(parent)
 {
+    updateAudioObject(); // Initial update of ready status
+}
 
+PlayerControl::~PlayerControl()
+{
+    stop();
+
+    if (audioOutput_ != nullptr) {
+        delete audioOutput_;
+    }
+}
+
+bool PlayerControl::audioOutputReady() const
+{
+    return audioOutput_ != nullptr;
+}
+
+QString PlayerControl::audioOutputReadyStatus() const
+{
+    return audioOutputReadyStatus_;
 }
--- eventsequencerlib/playercontrol.h
+++ eventsequencerlib/playercontrol.h
@@ -3,14 +3,34 @@
 
 #include "audiocontrol.h"
 
+class QAudioOutput;
+
 class PlayerControl : public AudioControl
 {
     Q_OBJECT
 
-protected:
+    QAudioOutput* audioOutput_ = nullptr;
+    QString audioOutputReadyStatus_;
     void updateAudioObject() override;
+    Q_PROPERTY(bool audioOutputReady          READ audioOutputReady       NOTIFY audioOutputReadyStatusChanged)
+    Q_PROPERTY(QString audioOutputReadyStatus READ audioOutputReadyStatus NOTIFY audioOutputReadyStatusChanged)
+
+    void updateAudioState();
+
 public:
     explicit PlayerControl(QObject* parent = nullptr);
+    ~PlayerControl() override;
+
+    bool audioOutputReady() const;
+    QString audioOutputReadyStatus() const;
+
+    Q_INVOKABLE void play();
+    Q_INVOKABLE void stop();
+
+signals:
+
+    void audioOutputReadyStatusChanged();
+
 };
 
 #endif // PLAYERCONTROL_H
