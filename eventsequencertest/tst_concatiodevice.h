502cb5d073e39069c75f059c4cd35e43b52c45bb
Sarah Wong <netcatkate@gmail.com>
2018-08-02 02:18:52 +0000

    Long test

--- eventsequencertest/tst_concatiodevice.h
+++ eventsequencertest/tst_concatiodevice.h
@@ -23,3 +23,32 @@ TEST(ConcatIODevice, Simple)
     QString str(ciod.readAll());
     EXPECT_EQ(str, "HelloWorld");
 }
+
+TEST(ConcatIODevice, Long)
+{
+    const int CHUNK_SIZE = 20000; // 16384 is how much Qt seems to read at once.
+
+    QByteArray ones(CHUNK_SIZE, '1');
+    QByteArray zeroes(CHUNK_SIZE, '0');
+
+    std::list<QIODevice*> inputs{new QBuffer(&ones), new QBuffer(&zeroes)};
+
+    for (QIODevice* iod : inputs) {
+        iod->open(QIODevice::ReadOnly);
+    }
+
+    ConcatIODevice ciod(&inputs);
+    ciod.open(QIODevice::ReadOnly);
+
+    QByteArray result = ciod.readAll();
+
+    std::vector<char> compareMe(CHUNK_SIZE * 2);
+    for (int i = 0; i < CHUNK_SIZE; ++i) {
+        compareMe[i] = '1';
+    }
+    for (int i = 0; i < CHUNK_SIZE; ++i) {
+        compareMe[CHUNK_SIZE + i] = '0';
+    }
+
+    EXPECT_THAT(result, testing::ElementsAreArray(compareMe));
+}
