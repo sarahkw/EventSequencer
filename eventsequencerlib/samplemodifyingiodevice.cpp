20a38ae0068afc25ed3b1fd3ea196bb326fd5943
Sarah Wong <netcatkate@gmail.com>
2018-08-17 16:23:13 +0000

    SMIOD inherits QIODevice

--- eventsequencerlib/samplemodifyingiodevice.cpp
+++ eventsequencerlib/samplemodifyingiodevice.cpp
@@ -1,6 +1,19 @@
 #include "samplemodifyingiodevice.h"
 
+qint64 SampleModifyingIODevice::readData(char *data, qint64 maxlen)
+{
+    return -1;
+}
+
+qint64 SampleModifyingIODevice::writeData(const char *data, qint64 len)
+{
+    Q_UNUSED(data);
+    Q_UNUSED(len);
+    return -1;
+}
+
 SampleModifyingIODevice::SampleModifyingIODevice()
 {
 
 }
+
--- eventsequencerlib/samplemodifyingiodevice.h
+++ eventsequencerlib/samplemodifyingiodevice.h
@@ -1,9 +1,15 @@
 #ifndef SAMPLEMODIFYINGIODEVICE_H
 #define SAMPLEMODIFYINGIODEVICE_H
 
+#include <QIODevice>
 
-class SampleModifyingIODevice
+class SampleModifyingIODevice : public QIODevice
 {
+protected:
+
+    qint64 readData(char *data, qint64 maxlen) override;
+    qint64 writeData(const char *data, qint64 len) override;
+
 public:
     SampleModifyingIODevice();
 };
