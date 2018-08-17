4070b1b4763095ebb3f2f7e51b54e5103558f144
Sarah Wong <netcatkate@gmail.com>
2018-08-17 02:09:32 +0000

    main.cpp: ifdef android for scout mode

--- EventSequencer/main.cpp
+++ EventSequencer/main.cpp
@@ -13,8 +13,11 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
 
-    bool scoutMode = true;
-    if (false) {
+    bool scoutMode = false;
+#ifdef Q_OS_ANDROID
+    scoutMode = true;
+#else
+    {
         QCommandLineParser parser;
         parser.addHelpOption();
         parser.addOptions({{{"s", "scout"}, "Start EvSeq Scout"}});
@@ -23,8 +26,11 @@ int main(int argc, char *argv[])
             scoutMode = true;
         }
     }
+#endif
 
-    // For now, scoutMode is used to prototype Android app.
+    // Fusion is a desktop-looking theme. Scout mode by nature shouldn't look
+    // like a desktop application, because if you're scouting, you should be
+    // mobile.
     if (!scoutMode) {
         QQuickStyle::setStyle("Fusion");
     }
