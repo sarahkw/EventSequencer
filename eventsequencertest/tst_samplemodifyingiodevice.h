70bc922a4874fca260eae8c81a92b23fe0e4330c
Sarah Wong <netcatkate@gmail.com>
2018-08-20 21:02:51 +0000

    tst: factor out reversefn

--- eventsequencertest/tst_samplemodifyingiodevice.h
+++ eventsequencertest/tst_samplemodifyingiodevice.h
@@ -10,19 +10,19 @@ inline void PrintTo(const QString& s, ::std::ostream *os)
     *os << s.toStdString();
 }
 
+
+const auto reverseFn = [](char* data, unsigned dataUnits,
+                          unsigned bytesPerUnit) {
+    for (unsigned i = 0; i < dataUnits; ++i) {
+        std::reverse(data + i * bytesPerUnit, data + (i + 1) * bytesPerUnit);
+    }
+};
+
 TEST(SampleModifyingIODevice, BasicRead)
 {
     QByteArray input = QString("FEEDME").toUtf8();
 
-    const auto modifyFn = [](char* data, unsigned dataUnits,
-                             unsigned bytesPerUnit) {
-        for (unsigned i = 0; i < dataUnits; ++i) {
-            std::reverse(data + i * bytesPerUnit,
-                         data + (i + 1) * bytesPerUnit);
-        }
-    };
-
-    SampleModifyingIODevice smiod(std::make_shared<QBuffer>(&input), 2, modifyFn);
+    SampleModifyingIODevice smiod(std::make_shared<QBuffer>(&input), 2, reverseFn);
     ASSERT_TRUE(smiod.open(QIODevice::ReadOnly));
     QString str(smiod.readAll());
     EXPECT_EQ(str, "EFDEEM");
@@ -33,18 +33,10 @@ TEST(SampleModifyingIODevice, BasicWrite)
 {
     QByteArray input = QString("AUTUMN").toUtf8();
 
-    const auto modifyFn = [](char* data, unsigned dataUnits,
-                             unsigned bytesPerUnit) {
-        for (unsigned i = 0; i < dataUnits; ++i) {
-            std::reverse(data + i * bytesPerUnit,
-                         data + (i + 1) * bytesPerUnit);
-        }
-    };
-
     auto output = std::make_shared<QBuffer>();
     ASSERT_TRUE(output->open(QIODevice::WriteOnly));
 
-    SampleModifyingIODevice smiod(output, 3, modifyFn);
+    SampleModifyingIODevice smiod(output, 3, reverseFn);
     ASSERT_TRUE(smiod.open(QIODevice::WriteOnly));
     EXPECT_THAT(smiod.write(input), input.size());
 
