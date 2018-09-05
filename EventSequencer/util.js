23b7556d45ee1101e3e43867bc6df011ddaac95e
Sarah Wong <netcatkate@gmail.com>
2018-09-05 03:32:49 +0000

    Don't use MDN trunc for licensing issues

--- EventSequencer/util.js
+++ EventSequencer/util.js
@@ -1,8 +1,9 @@
-// Taken from:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/trunc
 function trunc(v) {
-    v = +v;
-    return (v - v % 1)   ||   (!isFinite(v) || v === 0 ? v : v < 0 ? -0 : 0);
+    if (v > 0) {
+        return Math.floor(v)
+    } else {
+        return Math.ceil(v)
+    }
 }
 
 function negmod(a, b) {
